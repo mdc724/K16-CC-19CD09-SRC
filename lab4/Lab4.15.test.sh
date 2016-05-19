fe54d72d1133cdd5b0ea93fca012c5cc116e9aae
chris.nanda <chris.nanda@servicenow.com>
2016-05-19 11:29:57 -0700

    Moved Lab4 files

--- lab4/Lab4.15.test.sh
+++
@@ -1,6 +0,0 @@
-curl "https://<instance_name>/api/sn_k16_install/setup/user" \
---request POST \
---header "Accept:application/json"\
---header "Content-Type:application/json" \
---data "{\"username\":\"itest1\",\"password\":\"tester1\",\"displayName\":\"Test User\"}" \
---user 'admin':'admin'
